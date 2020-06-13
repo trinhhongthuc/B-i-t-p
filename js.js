abe599a4817edbd8721274efb4b726214c8cf449
trinh hong thuc <trinhhongthuc57@gmail.com>
2020-06-13 15:07:41 +0700

    add new ex

--- js.js
+++ js.js
@@ -53,9 +53,76 @@
 // mảng====================================
 //Viết chương trình chuyển đổi một số cố định thành một mảng các chữ số
 
-    function check( number ) {
-        var result = number.toString();
-        console.log(  [...result] );
-    }
+    // function check( number ) {
+    //     var result = number.toString();
+    //     console.log(  [...result] );
+    // }
+    // check( "123" );
 
-    check( "123" );
+
+    // Viết chương trình lọc các giá trị trong mảng và trả về mảng đã lọc mà không có giá trị đó
+
+    // function check( array ) {
+    //     var length = array.length;
+    //     for( let i = 0; i < length - 1; i++) {
+    //          for ( let j = i + 1; j < length; j++ ) {
+    //              if ( array[i] === array[j] ) {
+    //                  array.splice( j, 1);
+    //                  length--;
+    //                  j--;
+    //                  var result = array[i];
+    //              }
+    //          }
+
+    //          if( result === array[i]) {
+    //              array.splice( i, 1);
+    //              length--;
+    //              i--;
+    //          }
+    //     }
+
+    //     console.log( array );
+    // }   
+
+    // check( [ 1, 2, 5, 2, 6, 7, 8, 8] );
+
+
+    // Viết chương trình kết hợp tất cả các số của một mảng nhất định thành một mảng chứa tất cả các kết hợp đó 
+
+    // function create ( array ) {
+    //    var string = array.join("");
+    //    var arrayEmpty = [];
+    //     arrayEmpty.push( [] );
+    //     for( let i = 0; i < string.length; i++ ) {
+    //         var arrayEmpty2 = [];
+    //         arrayEmpty2.push( parseInt(  string[i]  ));
+    //         arrayEmpty.unshift( arrayEmpty2 );
+    //         for ( let j =  i + 1 ; j < string.length ; j++ ) {
+    //             arrayEmpty2 = []
+    //             var  result = string.slice( i, j + 1);
+    //             arrayEmpty2.push( parseInt( result) );
+    //             arrayEmpty.unshift( arrayEmpty2 );
+    //         }
+    //     }
+    //     console.log( arrayEmpty )
+    // }
+
+    // create ( [1, 2, 3, 4] );
+
+    // Viết chương trình trích xuất các giá trị tại vị trí chit mục 
+
+
+    // function display( array, ...number ) {
+    //     var length = array.length;
+    //     var arrayEmpty = [];
+    //     for ( let i = 0; i < number.length ; i ++ ) {
+    //         for( let j = 0; j < length; j++  ) {
+    //             if ( number[i] === j  ) {
+    //                 arrayEmpty.push( array[j] );
+    //             }
+    //         }
+    //     }
+    //     console.log( arrayEmpty );
+    // }
+
+    // display( [1, 5, 8, 3, 2], 0, 1, 2, 3);
